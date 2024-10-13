b8656ee4f1ff572f8309f82af08eaca4342b163e
quite2simple <simplechuck882@gmail.com>
2024-10-14 01:38:41 +0300

    Changed test to chirp things, spectogram converting and plotting it work fine

--- plotspectogram.py
+++ plotspectogram.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import librosa.display
 import numpy as np
+from scipy.signal import chirp
 from tospectogram import audio_to_log_mel_spectrogram
 
 SAMPLE_RATE = 22050
 AUDIO_DURATION = 1  # in seconds
 N_MELS = 128
 
-t = np.linspace(0, AUDIO_DURATION, int(SAMPLE_RATE * AUDIO_DURATION), endpoint=False)
-audio_fragment = np.random.normal(0, 1, int(SAMPLE_RATE * AUDIO_DURATION))
+t = np.linspace(0, AUDIO_DURATION, int(SAMPLE_RATE * AUDIO_DURATION))
+audio_fragment = chirp(t, f0=20, f1=SAMPLE_RATE / 2, t1=AUDIO_DURATION, method='linear')
+
 spectograms = audio_to_log_mel_spectrogram(audio_fragment, sample_rate=SAMPLE_RATE, n_mels=N_MELS)
 
 for spectogram in spectograms:
